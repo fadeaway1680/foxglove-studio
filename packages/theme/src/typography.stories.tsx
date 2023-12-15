e04e65bda892eeb3c4775165d93188dfffd6c6f5
Andrew Carr <andrew@foxglove.dev>
2023-12-15 11:11:31 +1100

    Simplify typography stories

--- packages/theme/src/typography.stories.tsx
+++ packages/theme/src/typography.stories.tsx
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Stack, Typography as MuiTypography } from "@mui/material";
+import { Stack, Typography as MuiTypography, TypographyVariant } from "@mui/material";
 import { StoryObj } from "@storybook/react";
 import { ReactNode } from "react";
 
@@ -14,84 +14,47 @@ function Wrapper({ children }: { children: ReactNode }): JSX.Element {
   return <Stack sx={{ border: "1px dotted", borderColor: "info.main" }}>{children}</Stack>;
 }
 
+const variants: { variant: TypographyVariant; text: string }[] = [
+  { variant: "h1", text: "h1. Heading" },
+  { variant: "h2", text: "h2. Heading" },
+  { variant: "h3", text: "h3. Heading" },
+  { variant: "h4", text: "h4. Heading" },
+  { variant: "h5", text: "h5. Heading" },
+  { variant: "h6", text: "h6. Heading" },
+  {
+    variant: "subtitle1",
+    text: "subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur",
+  },
+  {
+    variant: "subtitle2",
+    text: "subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur",
+  },
+  {
+    variant: "body1",
+    text: "body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.",
+  },
+  {
+    variant: "body2",
+    text: "body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.",
+  },
+  { variant: "button", text: "button text" },
+  { variant: "caption", text: "caption text" },
+  { variant: "overline", text: "overline text" },
+];
+
 export const Typography: StoryObj = {
   render: function Story() {
     return (
       <Stack gap={1} padding={1}>
-        <Wrapper>
-          <MuiTypography variant="h1" gutterBottom>
-            h1. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="h2" gutterBottom>
-            h2. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="h3" gutterBottom>
-            h3. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="h4" gutterBottom>
-            h4. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="h5" gutterBottom>
-            h5. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="h6" gutterBottom>
-            h6. Heading
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="subtitle1" gutterBottom>
-            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-            tenetur
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="subtitle2" gutterBottom>
-            subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-            tenetur
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="body1" gutterBottom>
-            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
-            numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="body2" gutterBottom>
-            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
-            numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="button" display="block" gutterBottom>
-            button text
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="caption" display="block" gutterBottom>
-            caption text
-          </MuiTypography>
-        </Wrapper>
-        <Wrapper>
-          <MuiTypography variant="overline" display="block" gutterBottom>
-            overline text
-          </MuiTypography>
-        </Wrapper>
+        {variants.map(({ variant, text }) => (
+          <Wrapper key={variant}>
+            <MuiTypography variant={variant} display="block" gutterBottom>
+              {text}
+            </MuiTypography>
+          </Wrapper>
+        ))}
       </Stack>
     );
   },
-
   parameters: { colorScheme: "light" },
 };
