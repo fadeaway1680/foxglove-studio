f3b7690c983d435985d8dac4b7dea928fb55d4ce
Caleb Foust <cfoust@sqweebloid.com>
2023-12-20 13:05:12 -0800

    feat: test for multiplex

---
+++ packages/den/workers/multiplex.test.ts
@@ -0,0 +1,34 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { multiplex } from "./.";
+
+describe("multiplex", () => {
+  it("creates a new instance when scheme returns undefined", () => {
+    let count = 0;
+    const create = multiplex<number>(
+      () => count++,
+      () => count--,
+      () => undefined,
+    );
+    const [instance, destroy] = create();
+    expect(instance).toEqual(0);
+    expect(count).toEqual(1);
+    destroy();
+    expect(count).toEqual(0);
+  });
+
+  it("creates a new instance when scheme returns index", () => {
+    let count = 0;
+    const create = multiplex<number>(
+      () => count++,
+      () => {},
+      () => 0,
+    );
+    create();
+    const [instance] = create();
+    expect(instance).toEqual(0);
+    expect(count).toEqual(1);
+  });
+});
